508613ad6679050f31b7824b1608c40978848f2a
randAyo <56219607+randAyo@users.noreply.github.com>
2020-09-03 17:58:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://test-project-5hq8k5ur4.vercel.app/';
+export const server = dev ? 'http://localhost:3000' : 'https://imprimerie.vercel.app/';
